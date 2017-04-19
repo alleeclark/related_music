b8239d52b2cf57ce0d75252cf2da18597f6562da
alleeclark <allee.clark@bison.howard.edu>
2017-04-19 13:58:18 -0700

    fixed lastfm

--- lastfm/artist_data.py
+++ lastfm/artist_data.py
@@ -3,57 +3,31 @@ import json
 import string
 from artist import Artist
 
-
 class lastfm_rest_service:
-    artist_dictionary = {}
 
-    def get_lastfm_url(artist_name):
+    def get_lastfm_url(self, artist_name):
         #url = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Rick Ross&api_key=bae243fa08eccb50f4884d67fdc54b20&format=json'
         lastfm_url ='http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=&api_key=bae243fa08eccb50f4884d67fdc54b20&format=json'
         for letter in artist_name:
             if letter not in string.ascii_letters:
-                return lastfm_url[:63] + artist_name + lastfm_url[63:]
-            else:
                 return False
+            else:
+                return lastfm_url[:63] + artist_name + lastfm_url[63:]
     
     def get_artist_info(self, artist_name):
-        response = requests.get(self.get_lastfm_url())
+        url = self.get_lastfm_url(artist_name)
+        response = requests.get(url)
         if response.status_code == requests.codes.OK:
-            # not parsing right 
-            artist_dic = response.json()['artist']
-            playcount = artist_dic['stats']['playcount']
-            listeners_count = artist_dic['stats']['listeners']
-            self.artist_dictionary[]
-            return self.artist_dictionary[artist_dic] =       
-            
+            artist_response = response.json()
+            lastfm_artist_name = artist_response['artist']['name']
+            playcount = artist_response['artist']['stats']['playcount']
+            listeners_count = artist_response['artist']['stats']['listeners']
+            # continue to add whatever attributes you want
+            # add them to some kind of key vlaue pair 
+            # return that dictionary
+        else:
+            return False
 
-
-    
-                
 brick = lastfm_rest_service()
-brick.get_lastfm_url()            
-
-#artist_list = ['100 Kila','100s','12 Gauge','2 Chainz']
-# def artist_data(artist_list):
-#     artist_dic_final = {}
-#     file = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=100 Kila&api_key=bae243fa08eccb50f4884d67fdc54b20&format=json'
-#     for i in range(len(artist_list)):
-#         #file text manipulation
-#         file_str = file.split('&')
-#         file_str[1] = 'artist=' + artist_list[i]
-#         file_str = '&'.join(file_str)
-        
-#         #last fm dictionary/data pull
-#         response = requests.get(file_str)
-#         artist_dic = response.json()['artist']
-#         play_count = artist_dic['stats']['playcount']
-#         listeners_count = artist_dic['stats']['listeners']
-        
-#         if not artist_list[i]  in artist_dic_final:
-#             artist_dic_final[artist_list[i]] = {}
-        
-#         artist_dic_final[artist_list[i]]['play_count'] = play_count
-#         artist_dic_final[artist_list[i]]['listeners_count'] = listeners_count
-        
-#     return artist_dic_final
-# print(artist_data(artist_list))
+name = "Rick Ross"
+brick.get_artist_info(name)
